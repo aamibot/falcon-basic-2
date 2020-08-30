623a1a60621b8a20ee90b0c859b804c5476be71c
aamibot <ameerhussainkh@gmail.com>
2020-08-30 19:32:34 +0530

    restructured api routes

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import falcon
-from falcon_auth import FalconAuthMiddleware, JWTAuthBackend
-from falcon_marshmallow import Marshmallow
-from api.routes.routes import routes
-from api.config.main_config import SECRET
+from api.routes.routes import get_app
 
-
-def user_loader(user):
-    return {"username": user}
-
-
-auth_backend = JWTAuthBackend(user_loader, SECRET, algorithm="HS256", leeway=30)
-auth_middleware = FalconAuthMiddleware(
-    auth_backend,
-    exempt_routes=["/auth", "/register"],
-    exempt_methods=["HEAD", "OPTIONS"],
-)
-
-api = falcon.API(
-    middleware=[Marshmallow(), auth_middleware]
-)  # middleware=[auth_middleware] - add to enable jwt authentication
-
-for endpoint, _object in routes:
-    api.add_route(endpoint, _object)
+api = get_app()
